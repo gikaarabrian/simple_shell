bf78d8fcbf2024a14d540cdb58b350d3e891c829
CHERIFA METIOUI <33041685+metiouicherifa@users.noreply.github.com>
2023-11-08 12:47:08 +0100

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -2,8 +2,8 @@
 
 /**
  * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * @info: Structure containing potential arguments. Used for maintaining
+ *      a constant function prototype.
  * Return: Always 0
  */
 char **get_environ(info_t *info)
@@ -19,8 +19,8 @@ char **get_environ(info_t *info)
 
 /**
  * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * @info: Structure containing potential arguments. Used for maintaining
+ *       a  constant function prototype.
  *  Return: 1 on delete, 0 otherwise
  * @var: the string env var property
  */
@@ -52,8 +52,8 @@ int _unsetenv(info_t *info, char *var)
 /**
  * _setenv - Initialize a new environment variable,
  *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * @info: Structure containing potential arguments. Used foro maintaining
+ *   a constant function prototype.
  * @var: the string env var property
  * @value: the string env var value
  *  Return: Always 0
@@ -91,4 +91,3 @@ int _setenv(info_t *info, char *var, char *value)
 	info->env_changed = 1;
 	return (0);
 }
-

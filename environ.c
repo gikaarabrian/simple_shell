4d88967f3afa64dd7321fbb832bdbc9341f9034c
CHERIFA METIOUI <33041685+metiouicherifa@users.noreply.github.com>
2023-11-08 12:41:22 +0100

    Update environ.c

--- environ.c
+++ environ.c
@@ -2,8 +2,8 @@
 
 /**
  * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * @info: a structure containing potential arguments. maintaining
+ * a constant function prototype.
  * Return: Always 0
  */
 int _myenv(info_t *info)
@@ -14,10 +14,9 @@ int _myenv(info_t *info)
 
 /**
  * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure containing potential arguments. Used for maintaining
  * @name: env var name
- *
- * Return: the value
+ * Return: the value.
  */
 char *_getenv(info_t *info, const char *name)
 {
@@ -35,10 +34,10 @@ char *_getenv(info_t *info, const char *name)
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * _mysetenv - Create a new environment variable,
+ * or modify an existing one
+ * @info: Structure containing potential arguments. Used for maintaining
+ *  a constant function prototype.
  *  Return: Always 0
  */
 int _mysetenv(info_t *info)
@@ -55,8 +54,8 @@ int _mysetenv(info_t *info)
 
 /**
  * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * @info: Structure containing potential arguments. Used for  maintaining
+ * a constant function prototype.
  * Return: Always 0
  */
 int _myunsetenv(info_t *info)
@@ -76,8 +75,8 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * @info: Structure containing potential arguments. Used for maintaining
+ * a constant function prototype.
  * Return: Always 0
  */
 int populate_env_list(info_t *info)
@@ -90,4 +89,3 @@ int populate_env_list(info_t *info)
 	info->env = node;
 	return (0);
 }
-
